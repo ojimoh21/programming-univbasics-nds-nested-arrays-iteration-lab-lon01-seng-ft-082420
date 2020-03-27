386deea2737d6b094ed0204ce8e5a169a88e876f
Omowonuola Jimoh <ojimoh21@live.co.uk>
2020-03-27 19:35:46 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -3,7 +3,10 @@ def join_ingredients(src)
   toppings = []
   row_index = 0 
   while row_index < src.count do 
-    toppings << `I love #{} and #{} on my pizza`
+    toppings << `I love #{src[row_index][0]} and #{src[row_index][1]} on my pizza`
+  end 
+  row_index += 1 
+  toppings
 end    
 
 def find_greater_pair(src)
