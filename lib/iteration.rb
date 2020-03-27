e11c4888d276dbaadf0e21e8dff8b17fa2c36a67
Omowonuola Jimoh <ojimoh21@live.co.uk>
2020-03-27 20:27:49 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -32,7 +32,7 @@ def total_even_pairs(src)
 total = 0 
 row_index = 0 
 while row_index < src.count do 
-  if src[row_index].even?
+  if src[row_index][0] % 2 == 0 && src[row_index][1] % 2 == 0 
     total += src[row_index][0] + src[row_index][1]
 end 
 row_index
